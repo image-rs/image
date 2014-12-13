27a9414d5acbde970f577454f3d1b1e58162f932
nwin <nwin@users.noreply.github.com>
2014-12-13 11:08:40 +0100

    add benchmark

--- src/imageops/sample.rs
+++ src/imageops/sample.rs
@@ -498,9 +498,21 @@ pub fn unsharpen<A: Primitive + 'static, T: Pixel<A>, I: GenericImage<T>>(
 
 #[cfg(test)]
 mod tests {
+    extern crate test;
 
+    use std::io::fs::File;
     use buffer::{ImageBuffer, RgbImage};
     use super::{resize, FilterType};
+
+
+    #[bench]
+    fn bench_resize(b: &mut test::Bencher) {
+        let img = ::open(&Path::new("./examples/fractal.png")).unwrap();
+        b.iter(|| {
+            test::black_box(super::resize( 200, 200, ::Nearest ));
+        });
+        b.bytes = 800*800*3 + 200*200*3;
+    }
 
     #[test]
     fn test_issue_186() {

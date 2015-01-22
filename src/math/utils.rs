52fe43246575732cc42ba8aa66e2ab1faaab179a
Łukasz Niemier <lukasz@niemier.pl>
2015-01-22 09:36:41 +0100

    Document code

--- src/math/utils.rs
+++ src/math/utils.rs
@@ -1,5 +1,12 @@
-use std::cmp::PartialOrd;
-
+/// Cut value to be inside given range
+///
+/// ```
+/// use image::math::utils;
+///
+/// assert_eq!(utils::clamp(-5, 0, 10),  0);
+/// assert_eq!(utils::clamp( 6, 0, 10),  6);
+/// assert_eq!(utils::clamp(15, 0, 10), 10);
+/// ```
 pub fn clamp<N>(a: N, min: N, max: N) -> N
 where N: PartialOrd {
     match () {

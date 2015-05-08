1db72b051ee52261e5c54e0078bb8f645f9212bf
nwin <nwin@users.noreply.github.com>
2015-05-08 10:30:48 +0200

    fix #403

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -10,7 +10,12 @@ use num::range_step;
 #[inline(always)]
 pub fn expand_packed<F>(buf: &mut [u8], channels: usize, bit_depth: u8, func: F)
 where F: Fn(u8, &mut[u8]) {
-    let entries = buf.len()/channels/(8/bit_depth as usize);
+	let pixels = buf.len()/channels*bit_depth as usize;
+	let extra = pixels % 8;
+    let entries = pixels / 8 + match extra {
+    	0 => 0,
+    	_ => 1
+    };
     let mask = ((1u16 << bit_depth) - 1) as u8;
     let i =
         (0..entries)
@@ -19,7 +24,8 @@ where F: Fn(u8, &mut[u8]) {
             // This has to be reversed to
             range_step(0, 8, bit_depth)
             .zip(repeat(idx))
-        );
+        )
+        .skip(extra);
     let channels = channels as isize;
     let j = range_step(buf.len() as isize - channels, -channels, -channels);
     //let j = range_step(0, buf.len(), channels).rev(); // ideal solution;

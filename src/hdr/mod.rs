6cb70e219aa2499ff8078225f266b87a1d3812da
red75prime <red75prim@gmail.com>
2016-06-24 13:52:21 +0500

    HDR encoder

--- src/hdr/mod.rs
+++ src/hdr/mod.rs
@@ -7,6 +7,11 @@
 //!  * http://www.graphics.cornell.edu/~bjw/rgbe/rgbe.c
 //!
 
+extern crate scoped_threadpool;
+
 mod hdr_decoder;
+mod hdr_encoder;
 
 pub use self::hdr_decoder::*;
+pub use self::hdr_encoder::*;
+

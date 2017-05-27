85a4460f6cddef9fca1db2be7a8367662857847e
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-05-27 11:38:03 +0100

    Add fuzzing scripts

---
+++ fuzz/fuzzers/fuzzer_script_tiff.rs
@@ -0,0 +1,7 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate image;
+
+fuzz_target!(|data: &[u8]| {
+    let _ = image::load_from_memory_with_format(data, image::ImageFormat::TIFF);
+});

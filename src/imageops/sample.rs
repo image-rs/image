2b1333e64da29bcba7cb75fc1229b0514a1517e0
Stephen Sherratt <sfsherratt@gmail.com>
2017-06-29 23:51:03 +1000

    Fix problems causing artifacts in image resizing
    These artifacts are noticeable when using the Nearest filter to resize
    images:
     - When resizing an image to its current size, the image becomes
       blurry.
     - When increasing the size of an image, it is also translated 1 pixel
       up and to the left, leaving black pixels along the right and bottom
       sides of the image.
    
    The root of the problem was in the application of filters in resizing
    operations. Specifically, it seemed like the calculation of the range of
    input pixels affecting output pixels, and the numeric argument given to
    filters, were incorrect.
    
    This commit also adds comments to the resizing functions explaining the
    calculations of these values.

--- src/imageops/sample.rs
+++ src/imageops/sample.rs
@@ -154,16 +154,53 @@ fn horizontal_sample<I, P, S>(image: &I, new_width: u32,
             1.0
         };
 
-        let filter_radius = (filter.support * filter_scale).ceil();
+        let filter_radius = filter.support * filter_scale;
 
         for outx in 0..new_width {
 
-            let inputx = (outx as f32 + 0.5) * ratio;
+            // Find the point in the input image corresponding to the centre
+            // of the current pixel in the output image.
+            //
+            // Then go half a pixel to the left (hence the `- 0.5`).
+            //
+            // The reason for subtracting 0.5 is because the filter kernel
+            // treats the centre of a pixel as 0. When finding the left and
+            // right limits below, we're interested in the range of input
+            // pixels whose colour can influence the colour of the current
+            // output pixel. This is equivalent to the range of input
+            // pixels for which inputx lies within the filter_radius-sized
+            // region centered at the centre of that pixel. Subtracting
+            // 0.5 here simplifies the rounding operations below.
+            //
+            let inputx = (outx as f32 + 0.5) * ratio - 0.5;
 
+            // Find the index of the left-most input pixel which can influence
+            // the colour of the current output pixel. A point on the right
+            // side of a pixel is considered to be part of that pixel.
             let left  = (inputx - filter_radius).ceil() as i64;
             let left  = clamp(left, 0, width as i64 - 1) as u32;
 
-            let right = (inputx + filter_radius).floor() as i64;
+            // Find the index of the right-most input pixel which can influence
+            // the colour of the current output pixel. A point on the left side
+            // of a pixel is NOT considered to be part of that pixel. This is
+            // important because:
+            //  - If we included the point in both neighbouring pixels it would
+            //    force the output pixel to be influenced be both input pixels,
+            //    and force filters which don't desire this (e.g. Nearest),
+            //    to make sure they only sample from one side in such cases.
+            //  - If we included the point in neither neighbouring pixel it
+            //    would cause output pixels corresponding to input pixel
+            //    boundaries to be black regardless of the input pixel colours.
+            //
+            // The choice of right vs left is arbitrary.
+            let right = {
+                let real_right = inputx + filter_radius;
+                if real_right.fract() == 0.0 {
+                    (real_right - 1.0) as i64
+                } else {
+                    real_right.floor() as i64
+                }
+            };
             let right = clamp(right, 0, width as i64 - 1) as u32;
 
             let mut sum = (0., 0., 0., 0.);
@@ -234,15 +271,27 @@ fn vertical_sample<I, P, S>(image: &I, new_height: u32,
             1.0
         };
 
-        let filter_radius = (filter.support * filter_scale).ceil();
+        let filter_radius = filter.support * filter_scale;
 
         for outy in 0..new_height {
-            let inputy = (outy as f32 + 0.5) * ratio;
+
+            // For an explanation of this algorithm, see the comments
+            // in horizontal_sample.
+
+            let inputy = (outy as f32 + 0.5) * ratio - 0.5;
 
             let left  = (inputy - filter_radius).ceil() as i64;
             let left  = clamp(left, 0, height as i64 - 1) as u32;
 
-            let right = (inputy + filter_radius).floor() as i64;
+            let right = {
+                // A point above a pixel is NOT part of that pixel.
+                let real_right = inputy + filter_radius;
+                if real_right.fract() == 0.0 {
+                    (real_right - 1.0) as i64
+                } else {
+                    real_right.floor() as i64
+                }
+            };
             let right = clamp(right, 0, height as i64 - 1) as u32;
 
             let mut sum = (0., 0., 0., 0.);

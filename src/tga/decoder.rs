f8a2905613f037946616357a7fe352bce51e156a
Martin Lindhe <martin-commit@ubique.se>
2018-12-06 16:44:09 +0100

    tga: recognize 32 bpp images as RGBA(8), fixes #838

--- src/tga/decoder.rs
+++ src/tga/decoder.rs
@@ -252,6 +252,7 @@ impl<R: Read + Seek> TGADecoder<R> {
         match (num_alpha_bits, other_channel_bits, color) {
             // really, the encoding is BGR and BGRA, this is fixed
             // up with `TGADecoder::reverse_encoding`.
+            (0, 32, true) => self.color_type = ColorType::RGBA(8),
             (8, 24, true) => self.color_type = ColorType::RGBA(8),
             (0, 24, true) => self.color_type = ColorType::RGB(8),
             (8, 8, false) => self.color_type = ColorType::GrayA(8),

eaeefa88a3f0508a9e7bd3b6e81db8bb034814c1
Birk Tjelmeland <birktjelmeland@yahoo.no>
2019-02-01 14:56:25 +0100

    Implemented incremental png loading

--- src/png.rs
+++ src/png.rs
@@ -10,10 +10,85 @@ extern crate png;
 
 use self::png::HasParameters;
 
-use std::io::{self, Cursor, Read, Write};
+use std::io::{self, Read, Write};
 
 use color::ColorType;
 use image::{ImageDecoder, ImageError, ImageResult};
+
+/// PNG Reader
+///
+/// This reader will try to read the png one row at a time,
+/// however for interlaced png files this is not posible and
+/// these are therefore readed at once.
+pub struct PNGReader<R: Read> {
+    reader: png::Reader<R>,
+    buffer: Vec<u8>,
+    index: usize,
+}
+
+impl<R: Read> PNGReader<R> {
+    fn new(mut reader: png::Reader<R>) -> ImageResult<PNGReader<R>> {
+        let len = reader.output_buffer_size();
+        // Since interlaced images do not come in 
+        // scanline order it is almost impossible to
+        // read them in a streaming fashion, however
+        // this shouldn't be a too big of a problem
+        // as most interlaced images should fit in memory.
+        let buffer = if reader.info().interlaced {
+            let mut buffer = vec![0; len];
+            reader.next_frame(&mut buffer)?;
+            buffer
+        } else {
+            Vec::new()
+        };
+
+        Ok(PNGReader {
+            reader,
+            buffer,
+            index: 0,
+        })
+    }
+}
+
+impl<R: Read> Read for PNGReader<R> {
+    fn read(&mut self, mut buf: &mut [u8]) -> io::Result<usize> {
+        // io::Write::write for slice cannot fail
+        let readed = buf.write(&self.buffer[self.index..]).unwrap();
+
+        let mut bytes = readed;
+        self.index += readed;
+    
+        while self.index + 1 >= self.buffer.len() {
+            match self.reader.next_row()? {
+                Some(row) => {
+                    // Faster to copy directly to external buffer
+                    let readed  = buf.write(row).unwrap();
+                    bytes += readed;
+
+                    self.buffer = (&row[readed..]).to_owned();
+                    self.index = 0;
+                }
+                None => return Ok(bytes)
+            }
+        }
+
+        Ok(bytes)
+    }
+
+    fn read_to_end(&mut self, buf: &mut Vec<u8>) -> io::Result<usize> {
+        let mut bytes = self.buffer.len();
+        buf.extend_from_slice(&self.buffer);
+        self.buffer = Vec::new();
+        self.index = 0;
+    
+        while let Some(row) = self.reader.next_row()? {
+            buf.extend_from_slice(row);
+            bytes += row.len();
+        }
+
+        Ok(bytes)
+    }
+}
 
 /// PNG decoder
 pub struct PNGDecoder<R: Read> {
@@ -33,7 +108,7 @@ impl<R: Read> PNGDecoder<R> {
 }
 
 impl<R: Read> ImageDecoder for PNGDecoder<R> {
-    type Reader = Cursor<Vec<u8>>;
+    type Reader = PNGReader<R>;
 
     fn dimensions(&self) -> (u64, u64) {
         let (w, h) = self.reader.info().size();
@@ -45,13 +120,19 @@ impl<R: Read> ImageDecoder for PNGDecoder<R> {
     }
 
     fn into_reader(self) -> ImageResult<Self::Reader> {
-        Ok(Cursor::new(self.read_image()?))
+        PNGReader::new(self.reader)
     }
 
     fn read_image(mut self) -> ImageResult<Vec<u8>> {
+        // This should be slightly faster than the default implementation
         let mut data = vec![0; self.reader.output_buffer_size()];
         self.reader.next_frame(&mut data)?;
         Ok(data)
+    }
+
+    fn scanline_bytes(&self) -> u64 {
+        let width = self.reader.info().width;
+        self.reader.output_line_size(width) as u64
     }
 }
 

829a590b48788c19cd3bd4f3f274dd01c8ecd358
Atul Bhosale <atul1bhosale@gmail.com>
2019-07-05 00:46:23 +0530

    Use 'dyn' since trait objects without an explicit 'dyn' are deprecated

--- src/decoder/stream.rs
+++ src/decoder/stream.rs
@@ -105,7 +105,7 @@ impl error::Error for DecodingError {
 
 impl fmt::Display for DecodingError {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(fmt, "{}", (self as &error::Error).description())
+        write!(fmt, "{}", (self as &dyn error::Error).description())
     }
 }
 

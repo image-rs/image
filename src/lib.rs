af0efc014727313e1f93dd830c29e47e9a3e77db
Ian Hamlin <ian@ihamlin.co.uk>
2019-07-22 13:50:08 +0100

    Change doc example to use set_* methods.

--- src/lib.rs
+++ src/lib.rs
@@ -25,15 +25,14 @@
 //! use std::path::Path;
 //! use std::fs::File;
 //! use std::io::BufWriter;
-//! // To use encoder.set()
-//! use png::HasParameters;
 //!
 //! let path = Path::new(r"/path/to/image.png");
 //! let file = File::create(path).unwrap();
 //! let ref mut w = BufWriter::new(file);
 //!
 //! let mut encoder = png::Encoder::new(w, 2, 1); // Width is 2 pixels and height is 1.
-//! encoder.set(png::ColorType::RGBA).set(png::BitDepth::Eight);
+//! encoder.set_color(png::ColorType::RGBA);
+//! encoder.set_depth(png::BitDepth::Eight);
 //! let mut writer = encoder.write_header().unwrap();
 //!
 //! let data = [255, 0, 0, 255, 0, 0, 0, 255]; // An array containing a RGBA sequence. First pixel is red and second pixel is black.

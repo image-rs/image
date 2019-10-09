1ab9e0757da4f9b190a9b4d85c1dd8b9a9c44752
Andreas Molzer <andreas.molzer@gmx.de>
2019-10-09 19:18:37 +0200

    Cleanup the chunk decoding implementation

--- src/decoder/stream.rs
+++ src/decoder/stream.rs
@@ -137,11 +137,22 @@ impl From<DecodingError> for io::Error {
 /// PNG StreamingDecoder (low-level interface)
 pub struct StreamingDecoder {
     state: Option<State>,
-    current_chunk: (Crc32, u32, Vec<u8>),
+    current_chunk: ChunkState,
     inflater: InflateStream,
     info: Option<Info>,
     current_seq_no: Option<u32>,
     have_idat: bool,
+}
+
+struct ChunkState {
+    /// Partial crc until now.
+    crc: Crc32,
+
+    /// Remanining bytes to be read.
+    remaining: u32,
+
+    /// Non-decoded bytes in the chunk.
+    raw_bytes: Vec<u8>,
 }
 
 impl StreamingDecoder {
@@ -151,7 +162,7 @@ impl StreamingDecoder {
     pub fn new() -> StreamingDecoder {
         StreamingDecoder {
             state: Some(State::Signature(0, [0; 7])),
-            current_chunk: (Crc32::new(), 0, Vec::with_capacity(CHUNCK_BUFFER_SIZE)),
+            current_chunk: ChunkState::default(),
             inflater: zlib_stream(),
             info: None,
             current_seq_no: None,
@@ -162,9 +173,9 @@ impl StreamingDecoder {
     /// Resets the StreamingDecoder
     pub fn reset(&mut self) {
         self.state = Some(State::Signature(0, [0; 7]));
-        self.current_chunk.0 = Crc32::new();
-        self.current_chunk.1 = 0;
-        self.current_chunk.2.clear();
+        self.current_chunk.crc = Crc32::new();
+        self.current_chunk.remaining = 0;
+        self.current_chunk.raw_bytes.clear();
         self.inflater = zlib_stream();
         self.info = None;
         self.current_seq_no = None;
@@ -244,16 +255,16 @@ impl StreamingDecoder {
                             (val >> 8) as u8,
                             val as u8
                         ];
-                        self.current_chunk.0.reset();
-                        self.current_chunk.0.update(&type_str);
-                        self.current_chunk.1 = length;
+                        self.current_chunk.crc.reset();
+                        self.current_chunk.crc.update(&type_str);
+                        self.current_chunk.remaining = length;
                         goto!(
                             ReadChunk(type_str, true),
                             emit Decoded::ChunkBegin(length, type_str)
                         )
                     },
                     Crc(type_str) => {
-                        let sum = self.current_chunk.0.clone().finalize();
+                        let sum = self.current_chunk.crc.clone().finalize();
                         if CHECKSUM_DISABLED || val == sum {
                             goto!(
                                 State::U32(U32Value::Length),
@@ -295,7 +306,7 @@ impl StreamingDecoder {
                     },
                     chunk::fdAT => {
                         if let Some(seq_no) = self.current_seq_no {
-                            let mut buf = &self.current_chunk.2[..];
+                            let mut buf = &self.current_chunk.raw_bytes[..];
                             let next_seq_no = buf.read_be()?;
                             if next_seq_no != seq_no + 1 {
                                 return Err(DecodingError::Format(format!(
@@ -316,7 +327,7 @@ impl StreamingDecoder {
                     },
                     // Handle other chunks
                     _ => {
-                        if self.current_chunk.1 == 0 { // complete chunk
+                        if self.current_chunk.remaining == 0 { // complete chunk
                             Ok((0, self.parse_chunk(type_str)?))
                         } else {
                             goto!(
@@ -330,11 +341,11 @@ impl StreamingDecoder {
             },
             ReadChunk(type_str, clear) => {
                 if clear {
-                    self.current_chunk.2.clear();
+                    self.current_chunk.raw_bytes.clear();
                 }
-                if self.current_chunk.1 > 0 {
-                    let (ref mut crc, ref mut remaining, ref mut c_buf) = self.current_chunk;
-                    let buf_avail = c_buf.capacity() - c_buf.len();
+                if self.current_chunk.remaining > 0 {
+                    let ChunkState { crc, remaining, raw_bytes } = &mut self.current_chunk;
+                    let buf_avail = raw_bytes.capacity() - raw_bytes.len();
                     let bytes_avail = min(buf.len(), buf_avail);
                     let n = min(*remaining, bytes_avail as u32);
                     if buf_avail == 0 {
@@ -342,7 +353,7 @@ impl StreamingDecoder {
                     } else {
                         let buf = &buf[..n as usize];
                         crc.update(buf);
-                        c_buf.extend_from_slice(buf);
+                        raw_bytes.extend_from_slice(buf);
                         *remaining -= n;
                         if *remaining == 0 {
                             goto!(n as usize, PartialChunk(type_str
@@ -357,8 +368,9 @@ impl StreamingDecoder {
                 }
             }
             DecodeData(type_str, mut n) => {
-                let chunk_len = self.current_chunk.2.len();
-                let (c, data) = self.inflater.update(&self.current_chunk.2[n..])?;
+                let chunk_len = self.current_chunk.raw_bytes.len();
+                let chunk_data = &self.current_chunk.raw_bytes[n..];
+                let (c, data) = self.inflater.update(chunk_data)?;
                 image_data.extend_from_slice(data);
                 n += c;
                 if n == chunk_len && data.is_empty() && c == 0 {
@@ -424,7 +436,7 @@ impl StreamingDecoder {
 
     fn parse_fctl(&mut self)
     -> Result<Decoded, DecodingError> {
-        let mut buf = &self.current_chunk.2[..];
+        let mut buf = &self.current_chunk.raw_bytes[..];
         let next_seq_no = buf.read_be()?;
 
         // Asuming that fcTL is required before *every* fdAT-sequence
@@ -476,7 +488,7 @@ impl StreamingDecoder {
                 "acTL chunk appeared after first IDAT chunk".into()
             ))
         } else {
-            let mut buf = &self.current_chunk.2[..];
+            let mut buf = &self.current_chunk.raw_bytes[..];
             let actl = AnimationControl {
                 num_frames: buf.read_be()?,
                 num_plays: buf.read_be()?
@@ -489,7 +501,7 @@ impl StreamingDecoder {
     fn parse_plte(&mut self)
     -> Result<Decoded, DecodingError> {
         if let Some(info) = self.info.as_mut() {
-            info.palette = Some(self.current_chunk.2.clone())
+            info.palette = Some(self.current_chunk.raw_bytes.clone())
         }
         Ok(Decoded::Nothing)
     }
@@ -501,7 +513,7 @@ impl StreamingDecoder {
             let info = self.get_info_or_err()?;
             (info.color_type, info.bit_depth as u8)
         };
-        let vec = self.current_chunk.2.clone();
+        let vec = self.current_chunk.raw_bytes.clone();
         let len = vec.len();
         let info = match self.info {
             Some(ref mut info) => info,
@@ -558,7 +570,7 @@ impl StreamingDecoder {
                 "pHYs chunk appeared after first IDAT chunk".into()
             ))
         } else {
-            let mut buf = &self.current_chunk.2[..];
+            let mut buf = &self.current_chunk.raw_bytes[..];
             let xppu = buf.read_be()?;
             let yppu = buf.read_be()?;
             let unit = buf.read_be()?;
@@ -581,7 +593,7 @@ impl StreamingDecoder {
     fn parse_ihdr(&mut self)
     -> Result<Decoded, DecodingError> {
         // TODO: check if color/bit depths combination is valid
-        let mut buf = &self.current_chunk.2[..];
+        let mut buf = &self.current_chunk.raw_bytes[..];
         let width = buf.read_be()?;
         let height = buf.read_be()?;
         let bit_depth = buf.read_be()?;
@@ -634,6 +646,16 @@ impl StreamingDecoder {
             color_type,
             interlaced
         ))
+    }
+}
+
+impl Default for ChunkState {
+    fn default() -> Self {
+        ChunkState {
+            crc:Crc32::new(),
+            remaining: 0,
+            raw_bytes: Vec::with_capacity(CHUNCK_BUFFER_SIZE),
+        }
     }
 }
 

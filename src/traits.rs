aca824362b56e353af04f90d83052965963309f4
nabijaczleweli <nabijaczleweli@gmail.com>
2020-03-31 19:30:34 +0200

    Use <numeric>::{to,from}_be_bytes() for {read,write}_be() impls

--- src/traits.rs
+++ src/traits.rs
@@ -1,22 +1,27 @@
 use std::io;
 
-// Will be replaced by stdlib solution
-fn read_all<R: io::Read + ?Sized>(this: &mut R, buf: &mut [u8]) -> io::Result<()> {
-    let mut total = 0;
-    while total < buf.len() {
-        match this.read(&mut buf[total..]) {
-            Ok(0) => {
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "failed to read the whole buffer",
-                ))
+macro_rules! read_bytes_ext {
+    ($output_type:ty) => {
+        impl<W: io::Read + ?Sized> ReadBytesExt<$output_type> for W {
+            #[inline]
+            fn read_be(&mut self) -> io::Result<$output_type> {
+                let mut bytes = [0u8; std::mem::size_of::<$output_type>()];
+                self.read_exact(&mut bytes)?;
+                Ok(<$output_type>::from_be_bytes(bytes))
             }
-            Ok(n) => total += n,
-            Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
-            Err(e) => return Err(e),
         }
-    }
-    Ok(())
+    };
+}
+
+macro_rules! write_bytes_ext {
+    ($input_type:ty) => {
+        impl<W: io::Write + ?Sized> WriteBytesExt<$input_type> for W {
+            #[inline]
+            fn write_be(&mut self, n: $input_type) -> io::Result<()> {
+                self.write_all(&n.to_be_bytes())
+            }
+        }
+    };
 }
 
 /// Read extension to read big endian data
@@ -31,38 +36,8 @@ pub trait WriteBytesExt<T>: io::Write {
     fn write_be(&mut self, _: T) -> io::Result<()>;
 }
 
-impl<W: io::Read + ?Sized> ReadBytesExt<u8> for W {
-    #[inline]
-    fn read_be(&mut self) -> io::Result<u8> {
-        let mut byte = [0];
-        read_all(self, &mut byte)?;
-        Ok(byte[0])
-    }
-}
-impl<W: io::Read + ?Sized> ReadBytesExt<u16> for W {
-    #[inline]
-    fn read_be(&mut self) -> io::Result<u16> {
-        let mut bytes = [0, 0];
-        read_all(self, &mut bytes)?;
-        Ok((u16::from(bytes[0])) << 8 | u16::from(bytes[1]))
-    }
-}
+read_bytes_ext!(u8);
+read_bytes_ext!(u16);
+read_bytes_ext!(u32);
 
-impl<W: io::Read + ?Sized> ReadBytesExt<u32> for W {
-    #[inline]
-    fn read_be(&mut self) -> io::Result<u32> {
-        let mut bytes = [0, 0, 0, 0];
-        read_all(self, &mut bytes)?;
-        Ok((u32::from(bytes[0])) << 24
-            | (u32::from(bytes[1])) << 16
-            | (u32::from(bytes[2])) << 8
-            | u32::from(bytes[3]))
-    }
-}
-
-impl<W: io::Write + ?Sized> WriteBytesExt<u32> for W {
-    #[inline]
-    fn write_be(&mut self, n: u32) -> io::Result<()> {
-        self.write_all(&[(n >> 24) as u8, (n >> 16) as u8, (n >> 8) as u8, n as u8])
-    }
-}
+write_bytes_ext!(u32);

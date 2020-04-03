da042eea309e914dbd214e617857f83476de1274
Andreas Molzer <andreas.molzer@gmx.de>
2020-04-03 18:05:52 +0200

    Add library top-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,99 @@
 //! image encoders and decoders and basic image manipulation
 //! functions.
 //!
-//! Additional documentation can currently be found in the
-//! [README.md file which is most easily viewed on github](https://github.com/image-rs/image/blob/master/README.md).
+//! Additional documentation can currently also be found in the
+//! [README.md file which is most easily viewed on github](https://github.com/image-rs/image/blob/master/README.md). 
+//!
+//! [Jump forward to crate content](#reexports)
+//!
+//! # Overview
+//!
+//! There are two core problems for this library provides solutions: a unified interface for images
+//! encodings and simple generic buffers for their content. It's possible to use either feature
+//! without the other. The focus is on small and stable set of common operations that can be
+//! supplemented by other specialized crates. The library also prefers safe solutions with few
+//! dependencies.
+//!
+//! | Format | Decoding | Encoding |
+//! | ------ | -------- | -------- |
+//! | PNG    | All supported color types | Same as decoding |
+//! | JPEG   | Baseline and progressive | Baseline JPEG |
+//! | GIF    | Yes | Yes |
+//! | BMP    | Yes | RGB(8), RGBA(8), Gray(8), GrayA(8) |
+//! | ICO    | Yes | Yes |
+//! | TIFF   | Baseline(no fax support) + LZW + PackBits | RGB(8), RGBA(8), Gray(8) |
+//! | WebP   | Lossy(Luma channel only) | No |
+//! | PNM    | PBM, PGM, PPM, standard PAM | Yes |
+//! | DDS    | DXT1, DXT3, DXT5 | No |
+//! | TGA    | Yes | No |
+//! | farbfeld | Yes | Yes |
+//!
+//! ## Using images decoders
+//!
+//! There exists a huge variety of image formats that are concerned with efficiently encoding image
+//! pixel data and auxiliary meta data for many different purposes. The `image` library provides
+//! decoders for many common formats, depending on the active features. The best way to use them
+//! depends on your use case.
+//!
+//! * [`open`] is a very simple way to load images from the file system, automatically deducing the
+//!   format but offering little customization.
+//! * [`load_from_memory`], [`load_from_memory_with_format`] present a similar interface for images
+//!   whose encoded data is already present in memory.
+//! * [`io::Reader`] is a builder providing a superset of the functions. It offers both
+//!   customization and auto-deduction but is slightly more involved. The main benefit is that the
+//!   interface is easier to evolve.
+//! * [`ImageDecoder`] is a trait for querying meta data and reading image pixels into a generic
+//!   byte buffer. It also contains a `Read` adaptor for stream reading the pixels.
+//! * [`DynamicImage::from_decoder`] can be used for creating a buffer from a single specific or
+//!   any custom decoder implementing the [`ImageDecoder`] trait.
+//!
+//! [`open`]: #fn.open.html
+//! [`load_from_memory`]: #fn.load_from_memory.html
+//! [`load_from_memory_with_format`]: #fn.load_from_memory_with_format.html
+//! [`io::Reader`]: io/struct.Reader.html
+//! [`DynamicImage::from_decoder`]: enum.DynamicImage.html#method.from_decoder
+//! [`ImageDecoder`]: trait.ImageDecoder.html
+//!
+//! ## Using image encoders
+//!
+//! Encoding pixel data is supported for the majority of formats but not quite as broadly.
+//!
+//! * [`DynamicImage::save`] is converse of `open` and stores a `DynamicImage`.
+//! * [`DynamicImage::write_to`] can be used to encode an image into any writer, for example into a
+//!   vector of bytes in memory.
+//! * [`save_buffer`], [`save_buffer_with_format`] are a low-level interface for saving an image
+//!   in the file system where the library initializes the chosen encoder.
+//! * [`ImageEncoder`] is a trait for encoding a byte buffer of image data and the inverse of the
+//!   `ImageDecoder` interface.
+//!
+//! [`save_buffer`]: #fn.save_buffer.html
+//! [`save_buffer_with_format`]: #fn.save_buffer_with_format.html
+//! [`DynamicImage::save`]: enum.DynamicImage.html#method.save
+//! [`DynamicImage::write_to`]: enum.DynamicImage.html#method.write_to
+//! [`ImageEncoder`]: trait.ImageEncoder.html
+//!
+//! ## Image buffers
+//!
+//! The library adds containers for channel data which together form some representation of a 2D
+//! matrix of pixels. These are all statically typed to avoid misinterpretation of byte data (and
+//! since Rust has no standard safe encapsulation for reinterpreting byte slices as another type).
+//! The main traits [`GenericImageView`] and [`GenericImage`] model a view on a 2D-matrix of
+//! addressable pixels and a buffer of independently accessible pixels respectively.
+//!
+//! The two main types for owning pixel data are [`ImageBuffer`] and [`DynamicImage`]. Note that
+//! the latter is an enum over well-supported pixel types that also offers conversion
+//! functionality.
+//!
+//! Additionally, the [`flat`] module contains items for interoperability with generic channel
+//! matrices and foreign interface. While still strict typed these dynamically validate length and
+//! other layout assumptions required to provide the trait interface. While quite generic You
+//! should be prepared for a bit of boilerplate when using these types.
+//!
+//! [`GenericImageView`]: trait.GenericImageView.html
+//! [`GenericImage`]: trait.GenericImage.html
+//! [`ImageBuffer`]: struct.ImageBuffer.html
+//! [`DynamicImage`]: enum.DynamicImage.html
+//! [`flat`]: flat/index.html
 
 #![warn(missing_docs)]
 #![warn(unused_qualifications)]

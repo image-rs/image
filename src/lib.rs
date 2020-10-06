e72f8135552a44aace3a819f198b05e072381331
Jonathan Behrens <fintelia@gmail.com>
2020-10-06 17:51:47 -0400

    Streamline docs.rs documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
-//! This crate provides native rust implementations of
-//! image encoders and decoders and basic image manipulation
-//! functions.
-//!
-//! Additional documentation can currently also be found in the
-//! [README.md file which is most easily viewed on github](https://github.com/image-rs/image/blob/master/README.md).
-//!
-//! [Jump forward to crate content](#reexports)
-//!
 //! # Overview
+//!
+//! This crate provides native rust implementations of image encoding and decoding as well as some
+//! basic image manipulation functions. Additional documentation can currently also be found in the
+//! [README.md file which is most easily viewed on
+//! github](https://github.com/image-rs/image/blob/master/README.md).
 //!
 //! There are two core problems for which this library provides solutions: a unified interface for image
 //! encodings and simple generic buffers for their content. It's possible to use either feature
@@ -15,80 +11,58 @@
 //! supplemented by other specialized crates. The library also prefers safe solutions with few
 //! dependencies.
 //!
-//! | Format | Decoding | Encoding |
-//! | ------ | -------- | -------- |
-//! | PNG    | All supported color types | Same as decoding |
-//! | JPEG   | Baseline and progressive | Baseline JPEG |
-//! | GIF    | Yes | Yes |
-//! | BMP    | Yes | RGB(8), RGBA(8), Gray(8), GrayA(8) |
-//! | ICO    | Yes | Yes |
-//! | TIFF   | Baseline(no fax support) + LZW + PackBits | RGB(8), RGBA(8), Gray(8) |
-//! | WebP   | Lossy(Luma channel only) | No |
-//! | PNM    | PBM, PGM, PPM, standard PAM | Yes |
-//! | DDS    | DXT1, DXT3, DXT5 | No |
-//! | TGA    | Yes | RGB(8), RGBA(8), BGR(8), BGRA(8), Gray(8), GrayA(8) |
-//! | farbfeld | Yes | Yes |
+//! # High level API
 //!
-//! ## Using images decoders
+//! Load images using [`io::Reader`]:
 //!
-//! There exists a huge variety of image formats that are concerned with efficiently encoding image
-//! pixel data and auxiliary meta data for many different purposes. The `image` library provides
-//! decoders for many common formats, depending on the active features. The best way to use them
-//! depends on your use case.
+//! ```rust,no_run
+//! # use std::io::Cursor;
+//! use image::io::Reader as ImageReader;
+//! # fn main() -> Result<(), image::ImageError> {
+//! # let bytes = vec![0u8];
 //!
-//! * [`open`] is a very simple way to load images from the file system, automatically deducing the
-//!   format but offering little customization.
-//! * [`load_from_memory`], [`load_from_memory_with_format`] present a similar interface for images
-//!   whose encoded data is already present in memory.
-//! * [`io::Reader`] is a builder providing a superset of the functions. It offers both
-//!   customization and auto-deduction but is slightly more involved. The main benefit is that the
-//!   interface is easier to evolve.
-//! * [`ImageDecoder`] is a trait for querying meta data and reading image pixels into a generic
-//!   byte buffer. It also contains a `Read` adaptor for stream reading the pixels.
-//! * [`DynamicImage::from_decoder`] can be used for creating a buffer from a single specific or
-//!   any custom decoder implementing the [`ImageDecoder`] trait.
+//! let img = ImageReader::open("myimage.png")?.decode()?;
+//! let img2 = ImageReader::new(Cursor::new(bytes)).decode()?;
+//! # Ok(())
+//! # }
+//! ```
 //!
-//! [`open`]: fn.open.html
-//! [`load_from_memory`]: fn.load_from_memory.html
-//! [`load_from_memory_with_format`]: fn.load_from_memory_with_format.html
+//! And save them using [`save`] or [`write_to`] methods:
+//!
+//! ```rust,no_run
+//! # use std::io::Write;
+//! # use image::ImageOutputFormat;
+//! # use image::DynamicImage;
+//! # #[cfg(feature = "png")]
+//! # fn main() -> Result<(), image::ImageError> {
+//! # let img: DynamicImage = unimplemented!();
+//! # let img2: DynamicImage = unimplemented!();
+//! img.save("empty.jpg")?;
+//!
+//! let mut bytes: Vec<u8> = Vec::new();
+//! img2.write_to(&mut bytes, image::ImageOutputFormat::Png)?;
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! With default features, the crate includes support for [many common image formats](codecs/index.html#supported-formats).
+//!
+//! [`save`]: enum.DynamicImage.html#method.save
+//! [`write_to`]: enum.DynamicImage.html#method.write_to
 //! [`io::Reader`]: io/struct.Reader.html
-//! [`DynamicImage::from_decoder`]: enum.DynamicImage.html#method.from_decoder
-//! [`ImageDecoder`]: trait.ImageDecoder.html
 //!
-//! ## Using image encoders
+//! # Image buffers
 //!
-//! Encoding pixel data is supported for the majority of formats but not quite as broadly.
+//! The two main types for storing images:
+//! * [`ImageBuffer`] which holds statically typed image contents.
+//! * [`DynamicImage`] which is an enum over the supported ImageBuffer formats
+//!     and supports conversions between them.
 //!
-//! * [`DynamicImage::save`] is the converse of `open` and stores a `DynamicImage`.
-//! * [`DynamicImage::write_to`] can be used to encode an image into any writer, for example into a
-//!   vector of bytes in memory.
-//! * [`save_buffer`], [`save_buffer_with_format`] provide a low-level interface for saving an image
-//!   in the file system, where the library initializes the chosen encoder.
-//! * [`ImageEncoder`] is a trait for encoding a byte buffer of image data and the inverse of the
-//!   `ImageDecoder` interface.
-//!
-//! [`save_buffer`]: #fn.save_buffer.html
-//! [`save_buffer_with_format`]: #fn.save_buffer_with_format.html
-//! [`DynamicImage::save`]: enum.DynamicImage.html#method.save
-//! [`DynamicImage::write_to`]: enum.DynamicImage.html#method.write_to
-//! [`ImageEncoder`]: trait.ImageEncoder.html
-//!
-//! ## Image buffers
-//!
-//! The library adds containers for channel data which together form some representation of a 2D
-//! matrix of pixels. These are all statically typed to avoid misinterpretation of byte data (and
-//! since Rust has no standard safe encapsulation for reinterpreting byte slices as another type).
-//! The main traits [`GenericImageView`] and [`GenericImage`] model a view on a 2D-matrix of
-//! addressable pixels and a buffer of independently accessible pixels respectively.
-//!
-//! The two main types for owning pixel data are [`ImageBuffer`] and [`DynamicImage`]. Note that
-//! the latter is an enum over well-supported pixel types that also offers conversion
-//! functionality.
-//!
-//! Additionally, the [`flat`] module contains items for interoperability with generic channel
-//! matrices and foreign interface. While still strictly typed, these dynamically validate length and
-//! other layout assumptions required to provide the trait interface. While they are quite generic, you
-//! should be prepared for a bit of boilerplate when using these types.
+//! As well as a few more specialized options:
+//! * [`GenericImage`] trait for a mutable image buffer.
+//! * [`GenericImageView`] trait for read only references to a GenericImage.
+//! * [`flat`] module containing types for interoperability with generic channel
+//!     matrices and foreign interfaces.
 //!
 //! [`GenericImageView`]: trait.GenericImageView.html
 //! [`GenericImage`]: trait.GenericImage.html
@@ -96,28 +70,18 @@
 //! [`DynamicImage`]: enum.DynamicImage.html
 //! [`flat`]: flat/index.html
 //!
-//! ## A note on format specific features
+//! # Low level encoding/decoding API
 //!
-//! One of the main goals of `image` is stability, in runtime but also for programmers. This
-//! ensures that performance as well as safety fixes reach a majority of its user base with little
-//! effort. Re-exporting all details of its dependencies would run counter to this goal as it
-//! linked _all_ major version bumps between them and `image`. As such, we are wary of exposing too
-//! many details, or configuration options, that are not shared between different image formats.
+//! The [`ImageDecoder`] and [`ImageDecoderExt`] traits are implemented for many image file
+//! formats. They decode image data by directly on raw byte slices. Given an ImageDecoder, you can
+//! produce a DynamicImage via [`DynamicImage::from_decoder`].
 //!
-//! Nevertheless, the advantage of precise control is hard to ignore. We will thus consider
-//! _wrappers_, not direct re-exports, in either of the following cases:
+//! [`ImageEncoder`] provides the analogous functionality for encoding image data.
 //!
-//! 1. A standard specifies that configuration _x_ is required for decoders/encoders and there
-//!    exists an essentially canonical way to control it.
-//! 2. At least two different implementations agree on some (sub-)set of features in practice.
-//! 3. A technical argument including measurements of the performance, space benefits, or otherwise
-//!    objectively quantified benefits can be made, and the added interface is unlikely to require
-//!    breaking changes.
-//!
-//! Features that fulfill two or more criteria are preferred.
-//!
-//! Re-exports of dependencies that reach version `1` will be discussed when it happens.
-
+//! [`DynamicImage::from_decoder`]: enum.DynamicImage.html#method.from_decoder
+//! [`ImageDecoderExt`]: trait.ImageDecoderExt.html
+//! [`ImageDecoder`]: trait.ImageDecoder.html
+//! [`ImageEncoder`]: trait.ImageEncoder.html
 #![warn(missing_docs)]
 #![warn(unused_qualifications)]
 #![deny(unreachable_pub)]
@@ -209,6 +173,44 @@ pub mod io;
 pub mod flat;
 
 /// Encoding and decoding for various image file formats.
+///
+/// # Supported formats
+///
+/// | Format | Decoding | Encoding |
+/// | ------ | -------- | -------- |
+/// | PNG    | All supported color types | Same as decoding |
+/// | JPEG   | Baseline and progressive | Baseline JPEG |
+/// | GIF    | Yes | Yes |
+/// | BMP    | Yes | RGB8, RGBA8, Gray8, GrayA8 |
+/// | ICO    | Yes | Yes |
+/// | TIFF   | Baseline(no fax support) + LZW + PackBits | RGB8, RGBA8, Gray8 |
+/// | WebP   | Lossy(Luma channel only) | No |
+/// | PNM    | PBM, PGM, PPM, standard PAM | Yes |
+/// | DDS    | DXT1, DXT3, DXT5 | No |
+/// | TGA    | Yes | RGB8, RGBA8, BGR8, BGRA8, Gray8, GrayA8 |
+/// | farbfeld | Yes | Yes |
+///
+/// ## A note on format specific features
+///
+/// One of the main goals of `image` is stability, in runtime but also for programmers. This
+/// ensures that performance as well as safety fixes reach a majority of its user base with little
+/// effort. Re-exporting all details of its dependencies would run counter to this goal as it
+/// linked _all_ major version bumps between them and `image`. As such, we are wary of exposing too
+/// many details, or configuration options, that are not shared between different image formats.
+///
+/// Nevertheless, the advantage of precise control is hard to ignore. We will thus consider
+/// _wrappers_, not direct re-exports, in either of the following cases:
+///
+/// 1. A standard specifies that configuration _x_ is required for decoders/encoders and there
+///    exists an essentially canonical way to control it.
+/// 2. At least two different implementations agree on some (sub-)set of features in practice.
+/// 3. A technical argument including measurements of the performance, space benefits, or otherwise
+///    objectively quantified benefits can be made, and the added interface is unlikely to require
+///    breaking changes.
+///
+/// Features that fulfill two or more criteria are preferred.
+///
+/// Re-exports of dependencies that reach version `1` will be discussed when it happens.
 pub mod codecs {
     #[cfg(feature = "avif")]
     pub mod avif;

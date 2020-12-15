b697962d7e8faa82aaf2c5d54aa67bdb4b252b2f
philippeitis <33013301+philippeitis@users.noreply.github.com>
2020-12-15 03:20:44 -0800

    Add size hint for build_huff_lut
    Since huff_lut always has to be 256 items long, we can get rid of bounds checks by returning and storing Box<[(u8, 16); 256]> instead of Vec, which does not have a size known at compile-time.

--- src/codecs/jpeg/entropy.rs
+++ src/codecs/jpeg/entropy.rs
@@ -1,9 +1,9 @@
 /// Given an array containing the number of codes of each code length,
 /// this function generates the huffman codes lengths and their respective
 /// code lengths as specified by the JPEG spec.
-fn derive_codes_and_sizes(bits: &[u8]) -> (Vec<u8>, Vec<u16>) {
-    let mut huffsize = vec![0u8; 256];
-    let mut huffcode = vec![0u16; 256];
+fn derive_codes_and_sizes(bits: &[u8]) -> ([u8; 256], [u16; 256]) {
+    let mut huffsize = [0u8; 256];
+    let mut huffcode = [0u16; 256];
 
     let mut k = 0;
 
@@ -48,8 +48,8 @@ fn derive_codes_and_sizes(bits: &[u8]) -> (Vec<u8>, Vec<u16>) {
     (huffsize, huffcode)
 }
 
-pub(crate) fn build_huff_lut(bits: &[u8], huffval: &[u8]) -> Vec<(u8, u16)> {
-    let mut lut = vec![(17u8, 0u16); 256];
+pub(crate) fn build_huff_lut(bits: &[u8], huffval: &[u8]) -> [(u8, u16); 256] {
+    let mut lut = [(17u8, 0u16); 256];
     let (huffsize, huffcode) = derive_codes_and_sizes(bits);
 
     for (i, &v) in huffval.iter().enumerate() {

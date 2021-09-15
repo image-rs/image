51a4ddd2a544ea0baf28479f441d0c1f812f5934
pheasetris <pheasetris@ecs.vuw.ac.nz>
2021-09-15 12:36:24 +1200

    Added stuff to deal with transform.

--- src/codecs/webp/lossless.rs
+++ src/codecs/webp/lossless.rs
@@ -1,20 +1,51 @@
-use std::{error, fmt, io::Read};
+//! Decoding of lossless WebP images
+//! 
+//! [Lossless spec](https://developers.google.com/speed/webp/docs/webp_lossless_bitstream_specification#522_decoding_entropy-coded_image_data)
+
+
+use std::{error, fmt, io::Read, ops::{AddAssign, Shl}};
 
 use byteorder::{LittleEndian, ReadBytesExt};
 
 use crate::{ImageError, ImageFormat, ImageResult, error::DecodingError};
 
+const DISTANCE_MAP: [(i8, i8); 120] = [
+    (0, 1),  (1, 0),  (1, 1),  (-1, 1), (0, 2),  (2, 0),  (1, 2),  (-1, 2),
+    (2, 1),  (-2, 1), (2, 2),  (-2, 2), (0, 3),  (3, 0),  (1, 3),  (-1, 3),
+    (3, 1),  (-3, 1), (2, 3),  (-2, 3), (3, 2),  (-3, 2), (0, 4),  (4, 0),
+    (1, 4),  (-1, 4), (4, 1),  (-4, 1), (3, 3),  (-3, 3), (2, 4),  (-2, 4),
+    (4, 2),  (-4, 2), (0, 5),  (3, 4),  (-3, 4), (4, 3),  (-4, 3), (5, 0),
+    (1, 5),  (-1, 5), (5, 1),  (-5, 1), (2, 5),  (-2, 5), (5, 2),  (-5, 2),
+    (4, 4),  (-4, 4), (3, 5),  (-3, 5), (5, 3),  (-5, 3), (0, 6),  (6, 0),
+    (1, 6),  (-1, 6), (6, 1),  (-6, 1), (2, 6),  (-2, 6), (6, 2),  (-6, 2),
+    (4, 5),  (-4, 5), (5, 4),  (-5, 4), (3, 6),  (-3, 6), (6, 3),  (-6, 3),
+    (0, 7),  (7, 0),  (1, 7),  (-1, 7), (5, 5),  (-5, 5), (7, 1),  (-7, 1),
+    (4, 6),  (-4, 6), (6, 4),  (-6, 4), (2, 7),  (-2, 7), (7, 2),  (-7, 2),
+    (3, 7),  (-3, 7), (7, 3),  (-7, 3), (5, 6),  (-5, 6), (6, 5),  (-6, 5),
+    (8, 0),  (4, 7),  (-4, 7), (7, 4),  (-7, 4), (8, 1),  (8, 2),  (6, 6),
+    (-6, 6), (8, 3),  (5, 7),  (-5, 7), (7, 5),  (-7, 5), (8, 4),  (6, 7),
+    (-6, 7), (7, 6),  (-7, 6), (8, 5),  (7, 7),  (-7, 7), (8, 6),  (8, 7)
+];
+
 #[derive(Debug, Clone, Copy)]
 enum DecoderError {
     /// Signature of 0x2f not found
     LosslessSignatureInvalid(u8),
+    /// Version Number must be 0
+    VersionNumberInvalid(u8),
+
+    InvalidTransformType(u8),
 }
 
 impl fmt::Display for DecoderError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             DecoderError::LosslessSignatureInvalid(sig) => 
-                f.write_fmt(format_args!("Invalid lossless signature: {:#04X?}", sig)),
+                f.write_fmt(format_args!("Invalid lossless signature: {}", sig)),
+            DecoderError::VersionNumberInvalid(num) => 
+                f.write_fmt(format_args!("Invalid version number: {}", num)),
+            DecoderError::InvalidTransformType(val) => 
+                f.write_fmt(format_args!("Invalid transform type: {}", val)),
         }
     }
 }
@@ -27,10 +58,21 @@ impl From<DecoderError> for ImageError {
 
 impl error::Error for DecoderError {}
 
+#[derive(Debug, Clone, Copy)]
+enum TransformType {
+    PredictorTransform(u8),
+    ColorTransform(u8),
+    SubtractGreen,
+    ColorIndexingTransform(u8),
+}
+
 //Decodes lossless WebP images
+#[derive(Debug)]
 pub(crate) struct LosslessDecoder<R> {
     r: R,
     bit_reader: BitReader,
+    frame: LosslessFrame,
+    transform: Option<TransformType>,
 }
 
 impl<R: Read> LosslessDecoder<R> {
@@ -40,6 +82,8 @@ impl<R: Read> LosslessDecoder<R> {
         LosslessDecoder {
             r,
             bit_reader: BitReader::new(),
+            frame: Default::default(),
+            transform: None,
         }
     }
 
@@ -55,6 +99,65 @@ impl<R: Read> LosslessDecoder<R> {
         let mut buf = Vec::new();
         self.r.read_to_end(&mut buf)?;
         self.bit_reader.init(buf);
+
+        self.frame.width = self.bit_reader.read_bits::<u16>(14)+1;
+        self.frame.height = self.bit_reader.read_bits::<u16>(14)+1;
+
+        let _alpha_used = self.bit_reader.read_bits::<u8>(1);
+
+        let version_num = self.bit_reader.read_bits::<u8>(3);
+
+        if version_num != 0 {
+            return Err(DecoderError::VersionNumberInvalid(version_num).into());
+        }
+
+        self.read_transforms()?;
+        self.read_image_data()?;
+
+        Ok(())
+    }
+
+    fn read_transforms(&mut self) -> ImageResult<()> {
+        while self.bit_reader.read_bits::<u8>(1) != 0 {
+            let transform_type_val = self.bit_reader.read_bits::<u8>(2);
+            
+            let transform_type = match transform_type_val {
+                0 => {
+                    //predictor
+
+                    let size_bits = self.bit_reader.read_bits::<u8>(3) + 2;
+
+                    TransformType::PredictorTransform(size_bits)
+                }
+                1 => {
+                    //color transform
+
+                    let size_bits = self.bit_reader.read_bits::<u8>(3) + 2;
+
+                    TransformType::ColorTransform(size_bits)
+                }
+                2 => {
+                    //subtract green
+
+                    TransformType::SubtractGreen
+                }
+                3 => {
+                    let color_table_size = self.bit_reader.read_bits::<u8>(8) + 1;
+
+                    TransformType::ColorIndexingTransform(color_table_size)
+                }
+                _ => return Err(DecoderError::InvalidTransformType(transform_type_val).into()),
+            };
+            
+
+            //self.transform = Some(transform_type);
+        }
+
+        Ok(())
+    }
+
+    fn read_image_data(&mut self) -> ImageResult<()> {
+
 
         Ok(())
     }
@@ -80,12 +183,14 @@ impl BitReader {
         self.buf = buf;
     }
 
-    fn read_bits(&mut self, num: u8) -> u32 {
-        let mut value = 0u32;
+    fn read_bits<T>(&mut self, num: u8) -> T 
+        where T: num_traits::Unsigned + Shl<u8, Output = T> +
+            AddAssign<T> + From<bool> {
+        let mut value: T = T::zero();
         
         for i in 0..num {
             let bit_true = self.buf[self.index] & (1 << self.bit_count) != 0;
-            value += u32::from(bit_true) << i;
+            value += T::from(bit_true) << i;
             self.bit_count = if self.bit_count == 0 {
                 self.index += 1;
                 7
@@ -103,6 +208,8 @@ impl BitReader {
 struct LosslessFrame {
     width: u16,
     height: u16,
+
+    buf: Vec<u8>,
 }
 
 #[cfg(test)]
@@ -117,16 +224,12 @@ mod test {
         //10011100 01000001 11100001
         let buf = vec![0x9C, 0x41, 0xE1];
 
-        for val in buf.iter() {
-            println!("{:#b}", val);
-        }
-
         bit_reader.init(buf);
 
-        assert_eq!(bit_reader.read_bits(3), 1); //100
-        assert_eq!(bit_reader.read_bits(2), 3); //11
-        assert_eq!(bit_reader.read_bits(6), 17); //100010
-        assert_eq!(bit_reader.read_bits(10), 240); //0000111100
-        assert_eq!(bit_reader.read_bits(3), 4); //001
+        assert_eq!(bit_reader.read_bits::<u8>(3), 1); //100
+        assert_eq!(bit_reader.read_bits::<u8>(2), 3); //11
+        assert_eq!(bit_reader.read_bits::<u8>(6), 17); //100010
+        assert_eq!(bit_reader.read_bits::<u16>(10), 240); //0000111100
+        assert_eq!(bit_reader.read_bits::<u8>(3), 4); //001
     }
 }

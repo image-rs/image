5cccd34f87fe421c0364499addabaa756691f762
Aster <aster@vers.site>
2023-04-10 10:37:51 +0800

    Support `Serialize` for `DynamicImage`

---
+++ src/io/ser.rs
@@ -0,0 +1,41 @@
+use crate::{ColorType, DynamicImage, ImageBuffer, ImageEncoder, Pixel, RgbaImage};
+use num_traits::Zero;
+use serde::de::{MapAccess, Visitor};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::fmt::Formatter;
+
+impl Serialize for ColorType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let image_type = match self {
+            ColorType::L8 => "ImageLuma8",
+            ColorType::La8 => "ImageLumaA8",
+            ColorType::Rgb8 => "ImageRgb8",
+            ColorType::Rgba8 => "ImageRgba8",
+            ColorType::L16 => "ImageLuma16",
+            ColorType::La16 => "ImageLumaA16",
+            ColorType::Rgb16 => "ImageRgb16",
+            ColorType::Rgba16 => "ImageRgba16",
+            ColorType::Rgb32F => "ImageRgb32F",
+            ColorType::Rgba32F => "ImageRgba32F",
+            _ => unreachable!("Unsupported color type: {:?}", self),
+        };
+        serializer.serialize_str(image_type)
+    }
+}
+
+impl Serialize for DynamicImage {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut ser = serializer.serialize_struct("DynamicImage", 4);
+        ser.serialize_field("type", &self.color());
+        ser.serialize_field("width", &self.width());
+        ser.serialize_field("height", &self.height());
+        ser.serialize_field("data", &self.as_bytes());
+        ser.end()
+    }
+}

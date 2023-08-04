f776602eafca0e158038c382ce3f6eb398034cbe
Jonathan Behrens <fintelia@gmail.com>
2023-08-04 10:24:11 -0700

    Upgrade tiff (#1975)

--- src/codecs/tiff.rs
+++ src/codecs/tiff.rs
@@ -68,6 +68,7 @@ where
             }
             tiff::ColorType::GrayA(n) => return Err(err_unknown_color_type(n.saturating_mul(2))),
             tiff::ColorType::RGB(n) => return Err(err_unknown_color_type(n.saturating_mul(3))),
+            tiff::ColorType::YCbCr(n) => return Err(err_unknown_color_type(n.saturating_mul(3))),
             tiff::ColorType::RGBA(n) | tiff::ColorType::CMYK(n) => {
                 return Err(err_unknown_color_type(n.saturating_mul(4)))
             }

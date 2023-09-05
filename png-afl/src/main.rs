6bcc80ab7b76306dc2bb989ab0b1a9ea0b38c996
whtsht <whiteshirt0079@gmail.com>
2023-09-05 19:00:28 +0900

    Fix mismatched types

--- png-afl/src/main.rs
+++ png-afl/src/main.rs
@@ -7,14 +7,16 @@ extern crate png;
 #[inline(always)]
 fn png_decode(data: &[u8]) -> Result<(png::OutputInfo, Vec<u8>), ()> {
     let decoder = png::Decoder::new(data);
-    let (info, mut reader) = decoder.read_info().map_err(|_| ())?;
 
-    if info.buffer_size() > 5_000_000 {
+    let mut reader = decoder.read_info().map_err(|_| ())?;
+
+    let buffer_size = reader.output_buffer_size();
+    if buffer_size > 5_000_000 {
         return Err(());
     }
 
-    let mut img_data = vec![0u8; info.buffer_size()];
-    reader.next_frame(&mut img_data).map_err(|_| ())?;
+    let mut img_data = vec![0u8; buffer_size];
+    let info = reader.next_frame(&mut img_data).map_err(|_| ())?;
 
     Ok((info, img_data))
 }

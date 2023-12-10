e58822b863ea3c93a07a619094c6b365da494051
Jonas Schade <DerZade@gmail.com>
2023-12-10 17:00:40 +0100

    Show features in documentation

--- src/lib.rs
+++ src/lib.rs
@@ -119,6 +119,7 @@
 #![cfg_attr(all(test, feature = "benchmarks"), feature(test))]
 // it's a backwards compatibility break
 #![allow(clippy::wrong_self_convention, clippy::enum_variant_names)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 #[cfg(all(test, feature = "benchmarks"))]
 extern crate test;

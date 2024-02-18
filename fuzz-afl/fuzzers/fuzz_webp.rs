54e0184a3ef514247d32fecb7f78bb07945d46e7
Jonathan Behrens <fintelia@gmail.com>
2024-02-18 14:25:23 -0800

    Use cursor in another place

--- fuzz-afl/fuzzers/fuzz_webp.rs
+++ fuzz-afl/fuzzers/fuzz_webp.rs
@@ -1,12 +1,14 @@
 extern crate afl;
 extern crate image;
 
+use std::io::Cursor;
+
 use image::{DynamicImage, ImageDecoder};
 use image::error::{ImageError, ImageResult, LimitError, LimitErrorKind};
 
 #[inline(always)]
 fn webp_decode(data: &[u8]) -> ImageResult<DynamicImage> {
-    let decoder = image::codecs::webp::WebPDecoder::new(data)?;
+    let decoder = image::codecs::webp::WebPDecoder::new(Cursor::new(data))?;
     let (width, height) = decoder.dimensions();
 
     if width.saturating_mul(height) > 4_000_000 {

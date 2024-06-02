bb7eb754264a0994e5d33f596b52d0e5a930d7c3
ripytide <james.forsterer@gmail.com>
2024-06-02 14:34:25 +0100

    switch to pixeli crate

--- src/traits.rs
+++ src/traits.rs
@@ -3,6 +3,7 @@
 // Note copied from the stdlib under MIT license
 
 use num_traits::{Bounded, Num, NumCast};
+pub use pixeli::Pixel;
 use std::ops::AddAssign;
 
 use crate::{
@@ -171,7 +172,7 @@ impl Lerp for f32 {
 
 /// The pixel with an associated `ColorType`.
 /// Not all possible pixels represent one of the predefined `ColorType`s.
-pub trait PixelWithColorType: Pixel + private::SealedPixelWithColorType {
+pub trait PixelWithColorType: Pixel + private::Sealed {
     /// This pixel has the format of one of the predefined `ColorType`s,
     /// such as `Rgb8`, `La16` or `Rgba32F`.
     /// This is needed for automatically detecting
@@ -216,151 +217,20 @@ impl PixelWithColorType for LumaA<u16> {
 mod private {
     use crate::color::*;
 
-    pub trait SealedPixelWithColorType {}
-    impl SealedPixelWithColorType for Rgb<u8> {}
-    impl SealedPixelWithColorType for Rgb<u16> {}
-    impl SealedPixelWithColorType for Rgb<f32> {}
+    pub trait Sealed {}
+    impl Sealed for Rgb<u8> {}
+    impl Sealed for Rgb<u16> {}
+    impl Sealed for Rgb<f32> {}
 
-    impl SealedPixelWithColorType for Rgba<u8> {}
-    impl SealedPixelWithColorType for Rgba<u16> {}
-    impl SealedPixelWithColorType for Rgba<f32> {}
+    impl Sealed for Rgba<u8> {}
+    impl Sealed for Rgba<u16> {}
+    impl Sealed for Rgba<f32> {}
 
-    impl SealedPixelWithColorType for Luma<u8> {}
-    impl SealedPixelWithColorType for LumaA<u8> {}
+    impl Sealed for Luma<u8> {}
+    impl Sealed for LumaA<u8> {}
 
-    impl SealedPixelWithColorType for Luma<u16> {}
-    impl SealedPixelWithColorType for LumaA<u16> {}
-}
-
-/// A generalized pixel.
-///
-/// A pixel object is usually not used standalone but as a view into an image buffer.
-pub trait Pixel: Copy + Clone {
-    /// The scalar type that is used to store each channel in this pixel.
-    type Subpixel: Primitive;
-
-    /// The number of channels of this pixel type.
-    const CHANNEL_COUNT: u8;
-
-    /// Returns the components as a slice.
-    fn channels(&self) -> &[Self::Subpixel];
-
-    /// Returns the components as a mutable slice
-    fn channels_mut(&mut self) -> &mut [Self::Subpixel];
-
-    /// A string that can help to interpret the meaning each channel
-    /// See [gimp babl](http://gegl.org/babl/).
-    const COLOR_MODEL: &'static str;
-
-    /// Returns the channels of this pixel as a 4 tuple. If the pixel
-    /// has less than 4 channels the remainder is filled with the maximum value
-    #[deprecated(since = "0.24.0", note = "Use `channels()` or `channels_mut()`")]
-    fn channels4(
-        &self,
-    ) -> (
-        Self::Subpixel,
-        Self::Subpixel,
-        Self::Subpixel,
-        Self::Subpixel,
-    );
-
-    /// Construct a pixel from the 4 channels a, b, c and d.
-    /// If the pixel does not contain 4 channels the extra are ignored.
-    #[deprecated(
-        since = "0.24.0",
-        note = "Use the constructor of the pixel, for example `Rgba([r,g,b,a])` or `Pixel::from_slice`"
-    )]
-    fn from_channels(
-        a: Self::Subpixel,
-        b: Self::Subpixel,
-        c: Self::Subpixel,
-        d: Self::Subpixel,
-    ) -> Self;
-
-    /// Returns a view into a slice.
-    ///
-    /// Note: The slice length is not checked on creation. Thus the caller has to ensure
-    /// that the slice is long enough to prevent panics if the pixel is used later on.
-    fn from_slice(slice: &[Self::Subpixel]) -> &Self;
-
-    /// Returns mutable view into a mutable slice.
-    ///
-    /// Note: The slice length is not checked on creation. Thus the caller has to ensure
-    /// that the slice is long enough to prevent panics if the pixel is used later on.
-    fn from_slice_mut(slice: &mut [Self::Subpixel]) -> &mut Self;
-
-    /// Convert this pixel to RGB
-    fn to_rgb(&self) -> Rgb<Self::Subpixel>;
-
-    /// Convert this pixel to RGB with an alpha channel
-    fn to_rgba(&self) -> Rgba<Self::Subpixel>;
-
-    /// Convert this pixel to luma
-    fn to_luma(&self) -> Luma<Self::Subpixel>;
-
-    /// Convert this pixel to luma with an alpha channel
-    fn to_luma_alpha(&self) -> LumaA<Self::Subpixel>;
-
-    /// Apply the function ```f``` to each channel of this pixel.
-    fn map<F>(&self, f: F) -> Self
-    where
-        F: FnMut(Self::Subpixel) -> Self::Subpixel;
-
-    /// Apply the function ```f``` to each channel of this pixel.
-    fn apply<F>(&mut self, f: F)
-    where
-        F: FnMut(Self::Subpixel) -> Self::Subpixel;
-
-    /// Apply the function ```f``` to each channel except the alpha channel.
-    /// Apply the function ```g``` to the alpha channel.
-    fn map_with_alpha<F, G>(&self, f: F, g: G) -> Self
-    where
-        F: FnMut(Self::Subpixel) -> Self::Subpixel,
-        G: FnMut(Self::Subpixel) -> Self::Subpixel;
-
-    /// Apply the function ```f``` to each channel except the alpha channel.
-    /// Apply the function ```g``` to the alpha channel. Works in-place.
-    fn apply_with_alpha<F, G>(&mut self, f: F, g: G)
-    where
-        F: FnMut(Self::Subpixel) -> Self::Subpixel,
-        G: FnMut(Self::Subpixel) -> Self::Subpixel;
-
-    /// Apply the function ```f``` to each channel except the alpha channel.
-    fn map_without_alpha<F>(&self, f: F) -> Self
-    where
-        F: FnMut(Self::Subpixel) -> Self::Subpixel,
-    {
-        let mut this = *self;
-        this.apply_with_alpha(f, |x| x);
-        this
-    }
-
-    /// Apply the function ```f``` to each channel except the alpha channel.
-    /// Works in place.
-    fn apply_without_alpha<F>(&mut self, f: F)
-    where
-        F: FnMut(Self::Subpixel) -> Self::Subpixel,
-    {
-        self.apply_with_alpha(f, |x| x);
-    }
-
-    /// Apply the function ```f``` to each channel of this pixel and
-    /// ```other``` pairwise.
-    fn map2<F>(&self, other: &Self, f: F) -> Self
-    where
-        F: FnMut(Self::Subpixel, Self::Subpixel) -> Self::Subpixel;
-
-    /// Apply the function ```f``` to each channel of this pixel and
-    /// ```other``` pairwise. Works in-place.
-    fn apply2<F>(&mut self, other: &Self, f: F)
-    where
-        F: FnMut(Self::Subpixel, Self::Subpixel) -> Self::Subpixel;
-
-    /// Invert this pixel
-    fn invert(&mut self);
-
-    /// Blend the color of a given pixel into ourself, taking into account alpha channels
-    fn blend(&mut self, other: &Self);
+    impl Sealed for Luma<u16> {}
+    impl Sealed for LumaA<u16> {}
 }
 
 /// Private module for supertraits of sealed traits.

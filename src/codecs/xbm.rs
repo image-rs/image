5398ac3ca14eb8d972fb62802d6339b313c71316
Shun Sakai <sorairolake@protonmail.ch>
2024-10-10 17:49:02 +0900

    feat(xbm): Change to accept 8-bit grayscale image as image

--- src/codecs/xbm.rs
+++ src/codecs/xbm.rs
@@ -23,24 +23,31 @@ impl<W: Write> XbmEncoder<W> {
         Self { writer }
     }
 
-    /// Encodes the image `buf` that has dimensions `width` and `height`.
+    /// Encodes the image `buf` (grayscale) that has dimensions `width` and
+    /// `height`.
+    ///
+    /// Each pixel is considered black (`1`) if its value is less than 128, and
+    /// white (`0`) otherwise.
     ///
     /// # Panics
     ///
-    /// Panics if `width * height != buf.len()` or `buf` contains pixels other
-    /// than `0` or `1`.
+    /// Panics if `width * height != buf.len()`.
     #[track_caller]
     pub fn encode(mut self, name: &str, buf: &[u8], width: u32, height: u32) -> ImageResult<()> {
+        fn to_bit(pixel: &u8) -> u8 {
+            if pixel < &128 {
+                1
+            } else {
+                0
+            }
+        }
+
         let expected_buffer_len = u64::from(width) * u64::from(height);
         assert_eq!(
             expected_buffer_len,
             buf.len() as u64,
             "Invalid buffer length: expected {expected_buffer_len} got {} for {width}x{height} image",
             buf.len(),
-        );
-        assert!(
-            !buf.iter().any(|&p| p > 1),
-            "image contained pixels other than `0` or `1`"
         );
         self.writer
             .write_all(format!("#define {name}_width {width}\n").as_bytes())?;
@@ -49,10 +56,10 @@ impl<W: Write> XbmEncoder<W> {
         self.writer
             .write_all(format!("static unsigned char {name}_bits[] = {{\n").as_bytes())?;
         let mut pixels = Vec::with_capacity(12);
-        for bits_per_line in buf.chunks(width as usize) {
-            for bits in bits_per_line.chunks(8) {
+        for pixels_per_line in buf.chunks(width as usize) {
+            for eight_pixels in pixels_per_line.chunks(8) {
                 let mut byte = 0;
-                for (i, bit) in bits.iter().enumerate() {
+                for (i, bit) in eight_pixels.iter().map(to_bit).enumerate() {
                     byte |= bit << (7 - i);
                 }
                 byte = byte.reverse_bits();
@@ -91,7 +98,7 @@ impl<W: Write> ImageEncoder for XbmEncoder<W> {
         height: u32,
         color_type: ExtendedColorType,
     ) -> ImageResult<()> {
-        if color_type != ExtendedColorType::L1 {
+        if color_type != ExtendedColorType::L8 {
             return Err(ImageError::Unsupported(
                 UnsupportedError::from_format_and_kind(
                     ImageFormat::Xbm.into(),
@@ -113,13 +120,13 @@ mod tests {
     fn write() {
         let mut writer = [0; 132];
         // "B" (8x7)
-        let buf = b"\x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x01\x01\x01\x00\x00\x00\
-\x00\x00\x01\x00\x00\x01\x00\x00\
-\x00\x00\x01\x01\x01\x00\x00\x00\
-\x00\x00\x01\x00\x00\x01\x00\x00\
-\x00\x00\x01\x01\x01\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x00\x00";
+        let buf = b"\xff\xff\xff\xff\xff\xff\xff\xff\
+\xff\xff\x00\x00\x00\xff\xff\xff\
+\xff\xff\x00\xff\xff\x00\xff\xff\
+\xff\xff\x00\x00\x00\xff\xff\xff\
+\xff\xff\x00\xff\xff\x00\xff\xff\
+\xff\xff\x00\x00\x00\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\xff\xff";
         let expected = "#define image_width 8
 #define image_height 7
 static unsigned char image_bits[] = {
@@ -127,7 +134,7 @@ static unsigned char image_bits[] = {
 };
 ";
         XbmEncoder::new(&mut writer[..])
-            .write_image(buf, 8, 7, ExtendedColorType::L1)
+            .write_image(buf, 8, 7, ExtendedColorType::L8)
             .unwrap();
         assert_eq!(str::from_utf8(&writer).unwrap(), expected);
     }
@@ -136,20 +143,20 @@ static unsigned char image_bits[] = {
     fn write_16x14() {
         let mut writer = [0; 268];
         // "B" (16x14)
-        let buf = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+        let buf = b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff";
         let expected = "#define image_width 16
 #define image_height 14
 static unsigned char image_bits[] = {
@@ -159,7 +166,7 @@ static unsigned char image_bits[] = {
 };
 ";
         XbmEncoder::new(&mut writer[..])
-            .write_image(buf, 16, 14, ExtendedColorType::L1)
+            .write_image(buf, 16, 14, ExtendedColorType::L8)
             .unwrap();
         assert_eq!(str::from_utf8(&writer).unwrap(), expected);
     }
@@ -168,12 +175,12 @@ static unsigned char image_bits[] = {
     fn write_width_7() {
         let mut writer = [0; 126];
         // "I" (7x6)
-        let buf = b"\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x01\x01\x01\x00\x00\
-\x00\x00\x00\x01\x00\x00\x00\
-\x00\x00\x00\x01\x00\x00\x00\
-\x00\x00\x01\x01\x01\x00\x00\
-\x00\x00\x00\x00\x00\x00\x00";
+        let buf = b"\xff\xff\xff\xff\xff\xff\xff\
+\xff\xff\x00\x00\x00\xff\xff\
+\xff\xff\xff\x00\xff\xff\xff\
+\xff\xff\xff\x00\xff\xff\xff\
+\xff\xff\x00\x00\x00\xff\xff\
+\xff\xff\xff\xff\xff\xff\xff";
         let expected = "#define image_width 7
 #define image_height 6
 static unsigned char image_bits[] = {
@@ -181,7 +188,7 @@ static unsigned char image_bits[] = {
 };
 ";
         XbmEncoder::new(&mut writer[..])
-            .write_image(buf, 7, 6, ExtendedColorType::L1)
+            .write_image(buf, 7, 6, ExtendedColorType::L8)
             .unwrap();
         assert_eq!(str::from_utf8(&writer).unwrap(), expected);
     }
@@ -190,18 +197,18 @@ static unsigned char image_bits[] = {
     fn write_width_14() {
         let mut writer = [0; 240];
         // "I" (14x12)
-        let buf = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\
-\x00\x00\x00\x00\x01\x01\x01\x01\x01\x01\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+        let buf = b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\x00\x00\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\
+\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\xff\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\
+\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff";
         let expected = "#define image_width 14
 #define image_height 12
 static unsigned char image_bits[] = {
@@ -210,7 +217,30 @@ static unsigned char image_bits[] = {
 };
 ";
         XbmEncoder::new(&mut writer[..])
-            .write_image(buf, 14, 12, ExtendedColorType::L1)
+            .write_image(buf, 14, 12, ExtendedColorType::L8)
+            .unwrap();
+        assert_eq!(str::from_utf8(&writer).unwrap(), expected);
+    }
+
+    #[test]
+    fn write_boundary_value() {
+        let mut writer = [0; 132];
+        // "B" (8x7)
+        let buf = b"\x80\x80\x80\x80\x80\x80\x80\x80\
+\x80\x80\x7f\x7f\x7f\x80\x80\x80\
+\x80\x80\x7f\x80\x80\x7f\x80\x80\
+\x80\x80\x7f\x7f\x7f\x80\x80\x80\
+\x80\x80\x7f\x80\x80\x7f\x80\x80\
+\x80\x80\x7f\x7f\x7f\x80\x80\x80\
+\x80\x80\x80\x80\x80\x80\x80\x80";
+        let expected = "#define image_width 8
+#define image_height 7
+static unsigned char image_bits[] = {
+    0x00, 0x1c, 0x24, 0x1c, 0x24, 0x1c, 0x00,
+};
+";
+        XbmEncoder::new(&mut writer[..])
+            .write_image(buf, 8, 7, ExtendedColorType::L8)
             .unwrap();
         assert_eq!(str::from_utf8(&writer).unwrap(), expected);
     }
@@ -230,15 +260,6 @@ static unsigned char image_bits[] = {
         let mut writer = [];
         let buf = [0; 8];
         let _: ImageResult<()> =
-            XbmEncoder::new(&mut writer[..]).write_image(&buf, 16, 2, ExtendedColorType::L1);
-    }
-
-    #[test]
-    #[should_panic(expected = "image contained pixels other than `0` or `1`")]
-    fn write_invalid_pixels() {
-        let mut writer = [];
-        let buf = b"\x00\x01\x02\x03\x03\x02\x01\x00";
-        let _: ImageResult<()> =
-            XbmEncoder::new(&mut writer[..]).write_image(buf, 8, 1, ExtendedColorType::L1);
+            XbmEncoder::new(&mut writer[..]).write_image(&buf, 16, 2, ExtendedColorType::L8);
     }
 }

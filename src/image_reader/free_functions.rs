282b2bff32dbd82145fc96af85e466d718560d98
Jonathan Behrens <fintelia@gmail.com>
2025-02-10 19:54:27 -0800

    Cargo fmt

--- src/image_reader/free_functions.rs
+++ src/image_reader/free_functions.rs
@@ -173,11 +173,13 @@ pub(crate) fn guess_format_impl(buffer: &[u8]) -> Option<ImageFormat> {
             if buffer.starts_with(signature) {
                 return Some(format);
             }
-        } else if buffer.len() >= signature.len() && buffer
+        } else if buffer.len() >= signature.len()
+            && buffer
                 .iter()
                 .zip(signature.iter())
                 .zip(mask.iter().chain(iter::repeat(&0xFF)))
-                .all(|((&byte, &sig), &mask)| byte & mask == sig) {
+                .all(|((&byte, &sig), &mask)| byte & mask == sig)
+        {
             return Some(format);
         }
     }

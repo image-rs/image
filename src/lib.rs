dbd54679bbe279ff5548cf1ed58ea588e50394df
Zac Harrold <zac@harrold.com.au>
2025-05-08 15:53:28 +1000

    Leave `std` prelude for tests

--- src/lib.rs
+++ src/lib.rs
@@ -129,7 +129,7 @@
 #![warn(clippy::alloc_instead_of_core)]
 #![warn(clippy::std_instead_of_alloc)]
 #![warn(clippy::alloc_instead_of_core)]
-#![no_std]
+#![cfg_attr(not(test), no_std)]
 
 #[cfg(feature = "std")]
 extern crate std;

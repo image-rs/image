03d7cc6cc667c3e4f13219d174134e7634c12f66
Jonathan Behrens <fintelia@gmail.com>
2025-07-31 17:52:21 -0700

    EXIF encoding for PNG and WebP and make API consistent

--- src/io/encoder.rs
+++ src/io/encoder.rs
@@ -60,6 +60,26 @@ pub trait ImageEncoder {
         ))
     }
 
+    /// Set the EXIF metadata to use for the image.
+    ///
+    /// This function is a no-op for formats that don't support EXIF metadata.
+    /// For formats that do support EXIF metadata, the metadata will be embedded
+    /// in the image when it is saved.
+    ///
+    /// # Errors
+    ///
+    /// This function returns an error if the format does not support EXIF metadata or if the
+    /// encoder doesn't implement saving EXIF metadata yet.
+    fn set_exif_metadata(&mut self, exif: Vec<u8>) -> Result<(), UnsupportedError> {
+        let _ = exif;
+        Err(UnsupportedError::from_format_and_kind(
+            ImageFormatHint::Unknown,
+            UnsupportedErrorKind::GenericFeature(
+                "EXIF metadata is not supported for this format".into(),
+            ),
+        ))
+    }
+
     /// Convert the image to a compatible format for the encoder. This is used by the encoding
     /// methods on `DynamicImage`.
     ///

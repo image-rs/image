5c5c0f3049d2b69b9363b1874256a9568367ceac
A. Molzer <5550310+197g@users.noreply.github.com>
2025-08-20 18:35:51 +0200

    Add alpha mask utilities to ImageBuffer
    The respective methods on `DynamicImage` are blocked on the type
    representation. We can't use any specific pixel type if the goal is
    preserving the type but `DynamicImage` has no Luma32F variant.
    For applying a mask we would have to take the parameter type as a
    non-`DynamicImage` somehow? Feels weird, we should just resolve to add
    at least 32-bit Luma to the crate, even if we do not add 32-bit
    LumaAlpha.

--- src/images/buffer.rs
+++ src/images/buffer.rs
@@ -888,6 +888,25 @@ where
             color_hint: None, // TODO: the pixel type might contain P::COLOR_TYPE if it satisfies PixelWithColorType
         }
     }
+
+    /// Extract the alpha channel as a Luma image.
+    ///
+    /// If the pixel does not have an alpha channel, the value is filled with a fully opaque mask
+    /// using the maximum value of the corresponding subpixel type.
+    pub fn to_alpha_mask(&self) -> ImageBuffer<Luma<P::Subpixel>, Vec<P::Subpixel>> {
+        let pixels = self.inner_pixels().chunks_exact(P::CHANNEL_COUNT.into());
+        let mut mask = vec![<P::Subpixel as crate::Primitive>::DEFAULT_MAX_VALUE; pixels.len()];
+
+        if P::HAS_ALPHA {
+            for (p, alpha) in pixels.zip(mask.iter_mut()) {
+                // If the pixel has an alpha channel, use it.
+                *alpha = *p.last().unwrap();
+            }
+        }
+
+        ImageBuffer::from_vec(self.width, self.height, mask)
+            .expect("used the right pixel and channel count")
+    }
 }
 
 impl<P, Container> ImageBuffer<P, Container>
@@ -988,6 +1007,42 @@ where
     #[track_caller]
     pub fn put_pixel(&mut self, x: u32, y: u32, pixel: P) {
         *self.get_pixel_mut(x, y) = pixel;
+    }
+
+    /// Fill the alpha channel of this image from a Luma mask.
+    ///
+    /// Returns an [`ImageError::Parameter`] if the mask dimensions do not match the image
+    /// dimensions. Otherwise, if the pixel type does not have an alpha channel this is a no-op.
+    pub fn apply_alpha_mask<RhsContainer>(
+        &mut self,
+        mask: &ImageBuffer<Luma<P::Subpixel>, RhsContainer>,
+    ) -> ImageResult<()>
+    where
+        RhsContainer: Deref<Target = [P::Subpixel]>,
+    {
+        if (self.width, self.height) != (mask.width(), mask.height()) {
+            return Err(ImageError::Parameter(ParameterError::from_kind(
+                ParameterErrorKind::DimensionMismatch,
+            )));
+        }
+
+        if !P::HAS_ALPHA {
+            return Err(ImageError::Parameter(ParameterError::from_kind(
+                ParameterErrorKind::NoAlphaChannel,
+            )));
+        }
+
+        let pixels = self
+            .inner_pixels_mut()
+            .chunks_exact_mut(P::CHANNEL_COUNT.into());
+
+        let mask = mask.inner_pixels();
+        for (p, alpha) in pixels.zip(mask.iter()) {
+            // If the pixel has an alpha channel, use it.
+            *p.last_mut().unwrap() = *alpha;
+        }
+
+        Ok(())
     }
 }
 
@@ -2113,6 +2168,27 @@ mod test {
 
         let result = target.copy_from_color_space(&source, options);
         assert!(matches!(result, Err(crate::ImageError::Parameter(_))));
+    }
+
+    #[test]
+    fn alpha_mask_of_gray() {
+        let image: GrayImage = ImageBuffer::new(4, 4);
+        let mask = image.to_alpha_mask();
+        assert_eq!(mask.as_raw(), &[255; 16]);
+    }
+
+    #[test]
+    #[rustfmt::skip]
+    fn alpha_mask_extraction() {
+        let image: ImageBuffer<LumaA<u8>, _> = ImageBuffer::from_raw(4, 4, vec![
+            0, 1, 0, 2, 0, 3, 0, 4,
+            0, 5, 0, 6, 0, 7, 0, 8,
+            0, 9, 0, 10, 0, 11, 0, 12,
+            0, 13, 0, 14, 0, 15, 0, 16,
+        ]).unwrap();
+
+        let mask = image.to_alpha_mask();
+        assert_eq!(mask.as_raw(), &(1u8..17).collect::<Vec<_>>());
     }
 }
 

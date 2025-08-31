6004caeeb5bf7d12938debaec93ba918b485665f
Alex Touchet <26315797+atouchet@users.noreply.github.com>
2025-08-30 20:39:13 -0700

    Update some URLs

--- src/imageops/sample.rs
+++ src/imageops/sample.rs
@@ -27,13 +27,13 @@ use crate::{
 ///
 /// To test the different sampling filters on a real example, you can find two
 /// examples called
-/// [`scaledown`](https://github.com/image-rs/image/tree/master/examples/scaledown)
+/// [`scaledown`](https://github.com/image-rs/image/tree/main/examples/scaledown)
 /// and
-/// [`scaleup`](https://github.com/image-rs/image/tree/master/examples/scaleup)
+/// [`scaleup`](https://github.com/image-rs/image/tree/main/examples/scaleup)
 /// in the `examples` directory of the crate source code.
 ///
 /// Here is a 3.58 MiB
-/// [test image](https://github.com/image-rs/image/blob/master/examples/scaledown/test.jpg)
+/// [test image](https://github.com/image-rs/image/blob/main/examples/scaledown/test.jpg)
 /// that has been scaled down to 300x225 px:
 ///
 /// <!-- NOTE: To test new test images locally, replace the GitHub path with `../../../docs/` -->

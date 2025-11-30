64e72fd3a5c0fdc579c8c9fffad3356e0321cfb2
A. Molzer <5550310+197g@users.noreply.github.com>
2025-11-30 03:27:37 +0100

    Split ImageFile off ImageReader
    The latter interface will be something that interfaces and interacts
    with the underlying decoder. Introduces ImageDecoder::set_viewport as a
    stand-in for what is intended here: color, subsampling, gain map
    application will take more interaction, clarification of how the
    library will request their data, and adjustments to the layout
    definition by the decoder.

--- src/io.rs
+++ src/io.rs
@@ -13,17 +13,12 @@ pub(crate) mod free_functions;
 pub(crate) mod image_reader_type;
 pub(crate) mod limits;
 
-#[deprecated(note = "this type has been moved and renamed to image::ImageReader")]
-/// Deprecated re-export of `ImageReader` as `Reader`
-pub type Reader<R> = ImageReader<R>;
 #[deprecated(note = "this type has been moved to image::Limits")]
 /// Deprecated re-export of `Limits`
 pub type Limits = limits::Limits;
 #[deprecated(note = "this type has been moved to image::LimitSupport")]
 /// Deprecated re-export of `LimitSupport`
 pub type LimitSupport = limits::LimitSupport;
-
-pub(crate) use self::image_reader_type::ImageReader;
 
 /// Adds `read_exact_vec`
 pub(crate) trait ReadExt {

03a1f1223dd83185e34b77be1eaf6beacf001f90
laund <me@laund.moe>
2025-12-30 17:28:32 +0100

    Implement faster specialized RgbaImage::(from/into)_raw_bgra variants  for 1 byte per pixel buffers

---
+++ benches/bgra_to_rgba.rs
@@ -0,0 +1,30 @@
+use std::time::Duration;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use image::{ImageBuffer, Rgba, RgbaImage};
+
+pub fn bench_bgra_to_rgba(c: &mut Criterion) {
+    let width = 2048;
+    let height = 2048;
+    // Create src as a Rgba image, but later treat it as a Bgra buffer
+    let src = ImageBuffer::from_pixel(2048, 2048, Rgba([255u8, 0, 0, 255])).to_vec();
+    let mut group = c.benchmark_group("bgra_to_rgba");
+    group.measurement_time(Duration::from_secs(15));
+    group.bench_function("from_raw_bgra", |b| {
+        b.iter_batched(
+            || src.clone(),
+            |input| RgbaImage::from_raw_bgra(width, height, input),
+            criterion::BatchSize::LargeInput,
+        );
+    });
+    group.bench_function("from_raw_bgra_bytes", |b| {
+        b.iter_batched(
+            || src.clone(),
+            |input| RgbaImage::from_raw_bgra_bytes(width, height, input),
+            criterion::BatchSize::LargeInput,
+        );
+    });
+}
+
+criterion_group!(benches, bench_bgra_to_rgba);
+criterion_main!(benches);

6fe02217b0d749f870efebee53c608ba78d05fb3
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2026-01-29 02:09:08 +0000

    Add comment

--- src/codecs/avif/decoder.rs
+++ src/codecs/avif/decoder.rs
@@ -129,6 +129,7 @@ impl<R: Read> AvifDecoder<R> {
 fn convert_orientation(orig: ImageRotation) -> Orientation {
     match orig {
         ImageRotation::D0 => Orientation::NoTransforms,
+        // AVIF rotations are counter-clockwise (clocksilly)
         ImageRotation::D90 => Orientation::Rotate270,
         ImageRotation::D180 => Orientation::Rotate180,
         ImageRotation::D270 => Orientation::Rotate90,

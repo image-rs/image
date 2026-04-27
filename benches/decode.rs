image-rs/image

commit 0fbfd932e008c2371dfe3607dd08c030a4107ffb

GIF: Zip `pixels_mut()` for faster animation decoding (#2914)

benches/decode.rs
@@ -1,7 +1,7 @@
-use std::{fs, iter, path};
+use std::{fs, hint::black_box, iter, path};
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use image::ImageFormat;
+use image::{AnimationDecoder, ImageFormat};
 
 #[derive(Clone, Copy)]
 struct BenchDef {
@@ -31,6 +31,14 @@ fn load_all(c: &mut Criterion) {
         BenchDef {
             dir: &["gif", "simple"],
             files: &["alpha_gif_a.gif", "sample_1.gif"],
+            format: ImageFormat::Gif,
+        },
+        BenchDef {
+            dir: &["gif", "anim"],
+            files: &[
+                "large-gif-anim-full-frame-replace.gif",
+                "large-gif-anim-combine.gif",
+            ],
             format: ImageFormat::Gif,
         },
         BenchDef {
@@ -95,12 +103,34 @@ fn bench_load(c: &mut Criterion, def: &BenchDef) {
     for file_name in def.files {
         let path: path::PathBuf = paths.clone().chain(iter::once(file_name)).collect();
         let buf = fs::read(path).unwrap();
+        let buf = buf.as_slice();
+
         group.bench_function(file_name.to_owned(), |b| {
             b.iter(|| {
-                image::load_from_memory_with_format(&buf, def.format).unwrap();
+                let r = image::load_from_memory_with_format(black_box(buf), black_box(def.format));
+                black_box(r.unwrap());
             });
         });
+
+        if decode_animation(buf, def.format).unwrap_or(0) > 1 {
+            group.bench_function("Animation: ".to_owned() + file_name, |b| {
+                b.iter(|| {
+                    let r = decode_animation(black_box(buf), black_box(def.format));
+                    black_box(r.unwrap());
+                });
+            });
+        }
     }
+}
+
+fn decode_animation(buf: &[u8], format: ImageFormat) -> image::ImageResult<usize> {
+    let reader = std::io::Cursor::new(buf);
+    let frames = match format {
+        ImageFormat::Gif => image::codecs::gif::GifDecoder::new(reader)?.into_frames(),
+        ImageFormat::WebP => image::codecs::webp::WebPDecoder::new(reader)?.into_frames(),
+        _ => panic!("Unsupported format for animation decoding"),
+    };
+    Ok(frames.count())
 }
 
 const IMAGE_DIR: [&str; 3] = [".", "tests", "images"];

src/codecs/gif.rs
@@ -363,8 +363,10 @@ impl<R: Read> Iterator for GifFrameIterator<R> {
         let image_buffer = if (frame.left, frame.top) == (0, 0)
             && (self.width, self.height) == frame_buffer.dimensions()
         {
-            for (x, y, pixel) in frame_buffer.enumerate_pixels_mut() {
-                let previous_pixel = non_disposed_frame.get_pixel_mut(x, y);
+            for (pixel, previous_pixel) in frame_buffer
+                .pixels_mut()
+                .zip(non_disposed_frame.pixels_mut())
+            {
                 blend_and_dispose_pixel(frame.disposal_method, previous_pixel, pixel);
             }
             frame_buffer

commit 8da7de5b760f99481d7c096e5b84b56a8815099b

Fix panic on every benchmark introduced by #2914

benches/decode.rs
@@ -128,7 +128,7 @@ fn decode_animation(buf: &[u8], format: ImageFormat) -> image::ImageResult<usize
     let frames = match format {
         ImageFormat::Gif => image::codecs::gif::GifDecoder::new(reader)?.into_frames(),
         ImageFormat::WebP => image::codecs::webp::WebPDecoder::new(reader)?.into_frames(),
-        _ => panic!("Unsupported format for animation decoding"),
+        _ => return Ok(0),
     };
     Ok(frames.count())
 }
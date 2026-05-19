b159a9b0c93ba49c8d2381f38e7c6fc5cdbc93bb
A. Molzer <5550310+197g@users.noreply.github.com>
2026-05-19 11:00:53 +0200

    Cleanup ICO alpha mask application

--- src/codecs/ico/decoder.rs
+++ src/codecs/ico/decoder.rs
@@ -371,9 +371,15 @@ impl<R: BufRead + Seek> ImageDecoder for IcoDecoder<R> {
                 // the mask is not required. Unfortunately, Wikipedia does not have a citation
                 // for that claim, so we can't be sure which is correct.
                 if data_end >= image_end + mask_length {
+                    let rgba = buf.as_chunks_mut::<4>().0;
+                    let rows = rgba.chunks_exact_mut(width as usize);
+                    debug_assert_eq!(rows.len(), height as usize, "entry matches dimension");
+
                     // If there's an AND mask following the image, read and apply it.
-                    for y in 0..height {
+                    // This from the bottom up (in terms of our coordinates).
+                    for row in rows.rev() {
                         let mut x = 0;
+
                         for _ in 0..mask_row_bytes {
                             // Apply the bits of each byte until we reach the end of the row.
                             let mask_byte = r.read_u8()?;
@@ -381,10 +387,12 @@ impl<R: BufRead + Seek> ImageDecoder for IcoDecoder<R> {
                                 if x >= width {
                                     break;
                                 }
+
                                 if mask_byte & (1 << bit) != 0 {
                                     // Set alpha channel to transparent.
-                                    buf[((height - y - 1) * width + x) as usize * 4 + 3] = 0;
+                                    row[x as usize][3] = 0;
                                 }
+
                                 x += 1;
                             }
                         }

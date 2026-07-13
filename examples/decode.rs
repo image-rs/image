29a90d3b2eb7cfd96a6b1ffe781033f9e14393bf
M Stoeckl <code@mstoeckl.com>
2026-07-12 20:43:48 -0400

    Print more info in decode example

--- examples/decode.rs
+++ examples/decode.rs
@@ -6,12 +6,19 @@ fn main() {
     let from = if env::args_os().count() == 2 {
         env::args_os().nth(1).unwrap()
     } else {
-        println!("Please enter a from and into path.");
+        println!("Please enter the path to an image file.");
         std::process::exit(1);
     };
 
     // Use the open function to load an image from a Path.
     // ```open``` returns a dynamic image.
     let im = image::open(Path::new(&from)).unwrap();
-    println!("{}", im.as_bytes().len());
+    println!(
+        "size: {} x {} ({} bytes), type {:?}",
+        im.width(),
+        im.height(),
+        im.as_bytes().len(),
+        im.color(),
+    );
+    println!("color space: {:?}", im.color_space());
 }
